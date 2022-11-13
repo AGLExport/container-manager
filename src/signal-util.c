3af3f86f1fafab9f43f839f89e09ad5e263b31fd
Naoto Yamaguchi <naoto.yamaguchi@aisin.co.jp>
2022-11-13 15:52:06 +0900

    Reworking to signal handling subsystem
    Existing signal handling subsystem only to support event loop exit by SIGTERM.
    This patch improve subsystem to support user defined signal event handling.
    
    Signed-off-by: Naoto Yamaguchi <naoto.yamaguchi@aisin.co.jp>

--- src/signal-util.c
+++ src/signal-util.c
@@ -11,7 +11,13 @@
 
 #include <stdio.h>
 
+#define FAKE_SIGRTMAX	(128)
+typedef struct s_signal_util_manage {
+	signal_util_t  signal_behavior_table[FAKE_SIGRTMAX+1];
+	sd_event *event;
+} signal_util_manage_t;
 
+static signal_util_manage_t g_sigutil_mng;
 
 /**
  * Sub function for UNIX signal handling.
@@ -22,29 +28,35 @@
  * 				-2 argument error
  *				-1 internal error
  */
-static int sd_event_sigchld_handler(sd_event_source *s, const struct signalfd_siginfo *si, void *userdata)
+static int sd_event_signal_handler(sd_event_source *s, const struct signalfd_siginfo *si, void *userdata)
 {
+	signal_util_manage_t* sigutil_mng = (signal_util_manage_t*)userdata;
+	int signalnum = -1;
+	int ret = -1;
+
+	if (sigutil_mng == NULL) {
+		return -1;
+	}
 
 	fprintf(stderr,"signal %d from %d\n", si->ssi_signo, si->ssi_pid);
 
-	return 0;
-}
-/**
- * Sub function for UNIX signal handling.
- * Block SIGTERM, when this process receive SIGTERM, event loop will exit.
- *
- * @param [in]	event	Incetance of sd_event
- * @return int	 0 success
- * 				-2 argument error
- *				-1 internal error
- */
-static int sd_event_sigterm_handler(sd_event_source *s, const struct signalfd_siginfo *si, void *userdata)
-{
-	sd_event *event = (sd_event*)userdata;
+	signalnum = sd_event_source_get_signal(s);
 
-	fprintf(stderr,"signal %d from %d\n", si->ssi_signo, si->ssi_pid);
+	if (0 < signalnum && signalnum <= FAKE_SIGRTMAX) {
+		signal_util_t  *elem = &sigutil_mng->signal_behavior_table[signalnum];
 
-	(void) sd_event_exit(event, 0);
+		if (elem->signal == signalnum) {
+
+			if (elem->signal_notify != NULL){
+
+				ret = elem->signal_notify(si, elem->userdata);
+			}
+		}
+	}
+
+	if (ret < 0) {
+		(void) sd_event_exit(sigutil_mng->event, -1);
+	}
 
 	return 0;
 }
@@ -62,32 +74,41 @@ int signal_setup(sd_event *event, signal_util_t *util_array, int array_num)
 	sigset_t ss;
 	int ret = -1;
 
-	if (event == NULL)
+	if (event == NULL || util_array == NULL || array_num < 1)
 		return -2;
 
-	// If the correct arguments are given, these function will never fail.
+	// If the correct arguments are given, sigemptyset and sigaddset function will never fail.
 	(void) sigemptyset(&ss);
-	(void) sigaddset(&ss, SIGTERM);
-	(void) sigaddset(&ss, SIGCHLD);
 
-	// Block SIGTERM
+	for(int i=0; i < array_num; i++) {
+		int signalnum = util_array[i].signal;
+
+		if (0 < signalnum && signalnum <= FAKE_SIGRTMAX) {
+			(void) sigaddset(&ss, signalnum);
+		}
+	}
+
+	// Block signals
 	ret = pthread_sigmask(SIG_BLOCK, &ss, NULL);
 	if (ret < 0)
 		goto err_return;
 
-	// Block SIGCHLD
-	ret = pthread_sigmask(SIGCHLD, &ss, NULL);
-	if (ret < 0)
-		goto err_return;
+	for(int i=0; i < array_num; i++) {
+		int signalnum = util_array[i].signal;
 
-	ret = sd_event_add_signal(event, NULL, SIGTERM, sd_event_sigterm_handler, event);
-	if (ret < 0) {
-		goto err_return;
+		if (0 < signalnum && signalnum <= FAKE_SIGRTMAX) {
+			g_sigutil_mng.signal_behavior_table[signalnum].signal = signalnum;
+			g_sigutil_mng.signal_behavior_table[signalnum].userdata = util_array[i].userdata;
+			g_sigutil_mng.signal_behavior_table[signalnum].signal_notify = util_array[i].signal_notify;
+
+			ret = sd_event_add_signal(event, NULL, signalnum, sd_event_signal_handler, &g_sigutil_mng);
+			if (ret < 0) {
+				goto err_return;
+			}
+		}
 	}
-	ret = sd_event_add_signal(event, NULL, SIGCHLD, sd_event_sigchld_handler, NULL);
-	if (ret < 0) {
-		goto err_return;
-	}
+
+	g_sigutil_mng.event = event;
 
 	return 0;
 
