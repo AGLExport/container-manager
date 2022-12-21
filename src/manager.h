00ffc38f5d10ae9366591fc6231863f2fdbd4766
Naoto Yamaguchi <naoto.yamaguchi@aisin.co.jp>
2022-12-22 08:46:21 +0900

    Change role structure
    The role attribute will use in container exchange behavior.　
    The previous structure is difficult to realize backward compatibility.
    This patch change role structure to realize backward compatibility.
    
    Signed-off-by: Naoto Yamaguchi <naoto.yamaguchi@aisin.co.jp>

--- src/manager.h
+++ src/manager.h
@@ -20,9 +20,29 @@ struct s_container_manager_bridge_config {
 };
 typedef struct s_container_manager_bridge_config container_manager_bridge_config_t;
 
+struct s_container_config;
+typedef struct s_container_config container_config_t;
+
+struct s_container_manager_role_elem {
+	struct dl_list list;
+	container_config_t *cc;	 		/**< pointer to container config */
+	//--- internal control data
+};
+typedef struct s_container_manager_role_elem container_manager_role_elem_t;
+
+struct s_container_manager_role_config {
+	struct dl_list list;
+	char *name;	 		/**< role name */
+	//--- internal control data
+	struct dl_list container_list;
+};
+typedef struct s_container_manager_role_config container_manager_role_config_t;
+
 struct s_container_manager_config {
 	char *configdir;	//**< container config directory */
 	struct dl_list bridgelist;
+	//--- internal control data
+	struct dl_list role_list;
 };
 typedef struct s_container_manager_config container_manager_config_t;
 
